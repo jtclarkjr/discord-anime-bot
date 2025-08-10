15062315c5a07a5103587587206a4ba4f2e6ae09
J <jtclarkjr@ucla.edu>
2025-08-10 22:40:21 +0900

    Update formatters.go

--- go/internal/utils/formatters.go
+++ go/internal/utils/formatters.go
@@ -41,18 +41,6 @@ func FormatCountdown(timeUntil int) string {
 	return timeString
 }
 
-// FormatDate formats a date to user-friendly format (Discord timestamp)
-// Format: <t:timestamp:D> - Shows date only (e.g., "December 25, 2023")
-func FormatDate(date time.Time) string {
-	return fmt.Sprintf("<t:%d:D>", date.Unix())
-}
-
-// FormatTime formats time to user-friendly format (Discord timestamp)
-// Format: <t:timestamp:t> - Shows time only (e.g., "3:30 PM")
-func FormatTime(date time.Time) string {
-	return fmt.Sprintf("<t:%d:t>", date.Unix())
-}
-
 // FormatAirDate formats date and time for air date display (Discord timestamp)
 // Format: <t:timestamp:F> - Full date and time (e.g., "Monday, December 25, 2023 3:30 PM")
 func FormatAirDate(date time.Time) string {
@@ -69,53 +57,4 @@ func FormatCompactDateTime(date time.Time) string {
 // Format: <t:timestamp:R> - Relative time (e.g., "in 2 hours", "3 days ago")
 func FormatRelativeTimestamp(date time.Time) string {
 	return fmt.Sprintf("<t:%d:R>", date.Unix())
-}
-
-// FormatRelativeTime formats a time relative to now (e.g., "in 2 hours", "tomorrow")
-func FormatRelativeTime(t time.Time) string {
-	now := time.Now()
-	duration := t.Sub(now)
-
-	if duration < 0 {
-		return "already aired"
-	}
-
-	// Less than a minute
-	if duration < time.Minute {
-		return "in less than a minute"
-	}
-
-	// Less than an hour
-	if duration < time.Hour {
-		minutes := int(duration.Minutes())
-		if minutes == 1 {
-			return "in 1 minute"
-		}
-		return fmt.Sprintf("in %d minutes", minutes)
-	}
-
-	// Less than a day
-	if duration < 24*time.Hour {
-		hours := int(duration.Hours())
-		if hours == 1 {
-			return "in 1 hour"
-		}
-		return fmt.Sprintf("in %d hours", hours)
-	}
-
-	// Less than a week
-	if duration < 7*24*time.Hour {
-		days := int(duration.Hours() / 24)
-		if days == 1 {
-			return "tomorrow"
-		}
-		return fmt.Sprintf("in %d days", days)
-	}
-
-	// More than a week
-	weeks := int(duration.Hours() / (24 * 7))
-	if weeks == 1 {
-		return "in 1 week"
-	}
-	return fmt.Sprintf("in %d weeks", weeks)
 }
