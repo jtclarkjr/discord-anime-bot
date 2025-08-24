2124f8294ee0e1e5c1252a9e9f81a2b4b980425a
J <jtclarkjr@ucla.edu>
2025-08-24 12:34:01 +0900

    Update index.ts

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,6 +1,6 @@
 // Export all GraphQL queries from a central location
 export { SEARCH_ANIME_BY_ID } from './searchById'
-export { SEARCH_ANIME_BY_TEXT } from './searchByText'  
+export { SEARCH_ANIME_BY_TEXT } from './searchByText'
 export { GET_ANIME_DETAILS } from './animeDetails'
 export { GET_RELEASING_ANIME } from './releasingAnime'
 export { GET_SEASONAL_ANIME } from './seasonalAnime'
